88bbb20e04adc56bc37f54680d3ed913d1560fa9
Angel3245 <joseangelperezgarrido@gmail.com>
2023-06-13 17:06:47 +0200

    Add chatbot code

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,15 +1,9 @@
 import argparse
-import sys
 from pathlib import Path
-from text_generation.model_classes import ModelDispatcher
 from shared import *
-from text_generation.gpt2 import GPT2TextGenerator
-from text_generation.gpt3 import GPT3TextGenerator
-from text_generation.bloom import BloomTextGenerator, BloomPeftTextGenerator
-from text_generation.llama import LlamaPeftTextGenerator
-if sys.platform != "win32":
-    from text_generation.petals import PetalsTextGenerator
-from clean_data import clean_sentence
+from text_generation.bloom import BloomPeftTextGenerator
+
+from clean_data import clean
 
 if __name__ == "__main__":
 
@@ -17,43 +11,39 @@ if __name__ == "__main__":
                     description='Run a chatbot using a LLM',
                     epilog='Jose Angel Perez Garrido - 2023')
     parser.add_argument("-o", "--option", type=str, help="select an option: cli -> run a chatbot in command line view. (default: cli)", default="cli")
-    parser.add_argument("-m", "--model", type=str, help="select a pretrained model to load. Supported models: "+str(ModelDispatcher.get_supported_types())+" (default: gpt2)", default='gpt2')
-    parser.add_argument("-d", "--dataset", type=str, help="select a dataset. (default: MentalKnowledge)", default="MentalKnowledge")
+    #parser.add_argument("-m", "--model", type=str, help="select a pretrained model to load. Supported models: "+str(ModelDispatcher.get_supported_types())+" (default: gpt2)", default='gpt2')
+    #parser.add_argument("-d", "--dataset", type=str, help="select a dataset. (default: MentalKnowledge)", default="MentalKnowledge")
     #parser.add_argument("-t", "--template", type=str, help="select a template file to create prompts. See /file/templates")
     args = parser.parse_args()
 
     path = Path.cwd()
 
     if args.option == "cli":
-        # python app\chatbot.py -o ask -m gpt2
-
-        model_name = args.model
-
-        # Get model_type from dispatcher
-        model_type = ModelDispatcher.get_model_type(model_name)
+        # python app\chatbot.py -o cli
 
         # Load model from disk
-        model_path = F"{str(path)}/output/MentalKnowledge/"+model_type+"/"+model_name
-        print("Loading model from",model_path)
+        model_path = F"{str(path)}/file/chatbot_model"
+        print("Loading chatbot model from",model_path)
 
-        # Text generator classes
-        text_generators = {
-            "gpt2": GPT2TextGenerator,
-            "gpt3": GPT3TextGenerator,
-            "bloom": BloomPeftTextGenerator,
-            #"petals": PetalsTextGenerator,
-            "llama": LlamaPeftTextGenerator
-        }
-
-        # Get model text_generator class from type
-        text_generator = text_generators[model_type](model_path)
+        # Get BLOOM text_generation model
+        text_generator = BloomPeftTextGenerator(model_path)
 
         # Ask questions to chatbot and create responses
         while True:
             user_input = input("User: ")
             if user_input.lower() == 'exit':
                 break
-            response = text_generator.generate_response(user_input)
+
+            # Clean input sentence
+            user_input = clean(user_input)
+
+            # Analyze input and create predefined responses if needed
+            # Check if user_input words are higher than 3
+            if(len(user_input.split()) < 3):
+                response = "Sorry, I did not understand you. Could you explain it better?"
+            else:
+                response = text_generator.generate_response(user_input)
+            
             print(f"Chatbot: {response}")
 
     print("PROGRAM FINISHED")
